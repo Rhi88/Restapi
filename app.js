1f11df6f7bfd2e7988d74892a0429ba072a48232
Rhi88 <rhizdo_nop@hotmail.com>
2018-07-18 11:30:34 -0500

    Ahora usa Mongodb de forma local

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongoose = require ("mongoose");
 
 var routes = require("./routes/routes.js");
 
@@ -13,3 +14,8 @@ routes(app);
 var server = app.listen(3000 , function(){
     console.log("aplicacion corriendo en puerto", server.address().port);
 });
+
+mongoose.connect('mongodb://127.0.0.1:27017/todo-list', { useNewUrlParser : true})
+.then(()=> {console.log('Conectado a la base de Mongo')})
+.catch(()=>{ console.log('Error al conectarse a la base de datos de Mongo')})
+
